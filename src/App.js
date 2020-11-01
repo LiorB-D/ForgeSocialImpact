34c63c18b7c8a36779a75c6e9c340c874ef70441
Lior Ben-David <liorbd@Liors-MacBook-Air.local>
2020-11-01 10:51:41 -0500

    Searching

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import './App.css';
 import React, {useState, useEffect} from 'react';
+import Grid from '@material-ui/core/Grid';
+import TextField from '@material-ui/core/TextField';
 
-
-
-const url = "https://api.propublica.org/congress/v1/116/house/members.json"
+const url = "https://api.propublica.org/congress/v1/116/senate/members.json"
 const apiKey = "4ll6jGJJSLaZrafzsIuVFmaNrmcrQFlgyQzcy3Yi"
 
 function App() {
   const [members, setMembers] = useState([])
+  const [valMembers, setValMembers] = useState([])
+  const [searchStr, setSearchStr] = useState("")
   
+
     const fetchMembers = async () => {
       let resp = fetch(url, {
         headers: {
@@ -16,22 +19,50 @@ function App() {
         }
       }).then(response => response.json()).then(data => {
         setMembers(data.results[0].members)
-        console.log(members)
+        console.log(data)
 
       })
     }  
+
+    const updateSearch = (e) => {
+      setSearchStr(e.target.value)
+
+      let str = e.target.value.toLowerCase()
+      let membs = []
+      members.map((m) => {
+          if(m.first_name.toLowerCase().includes(str) || m.last_name.toLowerCase().includes(str) || m.state.toLowerCase().includes(str)) {
+            membs.push(m)
+          }
+      })
+      setValMembers(membs)
+
+    }
+
     useEffect(() => {
       fetchMembers()
     }, [])
-
+    
+    const MemberBox = (member) => {
+      return <div class = {"MemberBox" + member.member.party}>
+        <h3>{member.member.first_name} {member.member.last_name}</h3>
+        <h4>{member.member.party}-{member.member.state}</h4>
+        <p>{member.member.url}</p>
+        </div>
+    }
   
 
   return (
     <div className="App">
-      <h1>Senators</h1>
-      {members.map(m => (
-          <p>{m.last_name}</p>
+      <h1>Search Senators by Name or State Abbrev.</h1>
+      <TextField label = "Search Senators" value = {searchStr} onChange = {updateSearch}/>
+
+        <Grid container justify="center" spacing={3}>
+        {valMembers.map(m => (
+          <Grid item>
+            <MemberBox member = {m}/>
+          </Grid>
         ))}
+        </Grid>
     </div>
   );
 }
